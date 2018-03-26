2796c883273003a741a18affeb231d852f9868f4
geniusywj <37320896+geniusywj@users.noreply.github.com>
2018-03-26 20:30:38 +0800

    Update urls.py

--- imagex/imagex/urls.py
+++ imagex/imagex/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('search/', include('search.urls')),
     path('admin/', admin.site.urls),
+    path('upload/', include('upload.urls')),
 ]
