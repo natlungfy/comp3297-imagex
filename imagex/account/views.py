f8f18469db66ad2a853db30d2f1799b331d4dadd
Natalie Lung <lovya.natlung@gmail.com>
2018-04-11 02:15:24 +0800

    Add confirm popup in Delete; Implement Change password

--- imagex/account/views.py
+++ imagex/account/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'account/change_password.html', {
+        'form': form
+    })
--- imagex/imagex/urls.py
+++ imagex/imagex/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from search.views import download
+from account.views import change_password
 from django.urls import path
 from django.conf.urls import include, url
 from django.conf import settings
@@ -30,6 +31,7 @@ urlpatterns = [
                   path('upload/', include('upload.urls')),
                   path('profiles/', include('profiles.urls')),
                   url(r'^download/$', download, name="download"),
+                  url(r'^change_password/$', change_password, name='change_password'),
                   url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
                   url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
                   url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
