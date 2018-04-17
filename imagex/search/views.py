a989e8467c81c10b9706675c4d490a633913ebe5
Natalie Lung <lovya.natlung@gmail.com>
2018-04-17 23:21:22 +0800

    Implement sort ranking

--- imagex/search/views.py
+++ imagex/search/views.py
@@ -7,7 +7,7 @@ from wsgiref.util import FileWrapper
 
 
 def index(request):
-    all_images = Image.objects.all().order_by('-upload_date', '-id')
+    all_images = Image.objects.all().order_by('-id')
     all_cat = Category.CATEGORY
     all_photog = Member.objects.all()
     return render(request, 'index.html', {'images': all_images, 'all_cat': all_cat, 'all_photog': all_photog})
@@ -27,14 +27,25 @@ def search_keyword(request):
             for k in keywords:
                 images = images.filter(tag__tag_name__iexact=k)
 
+        if len(request.GET.keys()) > 1:
+            sort = request.GET.getlist('sort')[0]
+            if sort == 'recent':
+                images = images.order_by('-id')
+
         return render(request, 'search/results.html', {"q": q, "images": images,
                                                        'media_url': settings.MEDIA_URL})
 
 
 def search_category(request):
     if request.method == 'GET':
-        cat = list(request.GET.keys())[0]
+        cat = request.GET.getlist('cat')[0]
         images = Image.objects.filter(category__exact=cat)
+
+        if len(request.GET.keys()) > 1:
+            sort = request.GET.getlist('sort')[0]
+            if sort == 'recent':
+                images = images.order_by('-id')
+
         d = dict(Category.CATEGORY)
         cat_name = d[cat]
         return render(request, 'search/category.html', {'cat_name': cat_name, 'images': images,
@@ -43,8 +54,14 @@ def search_category(request):
 
 def search_photographer(request):
     if request.method == 'GET':
-        photog = list(request.GET.keys())[0]
+        photog = request.GET.getlist('photog')[0]
         images = Image.objects.filter(photographer__username__username__exact=photog)
+
+        if len(request.GET.keys()) > 1:
+            sort = request.GET.getlist('sort')[0]
+            if sort == 'recent':
+                images = images.order_by('-id')
+
         full_name = Member.objects.get(username=photog).username.first_name + " " + Member.objects.get(
             username=photog).username.last_name
         return render(request, 'search/photographer.html', {'full_name': full_name, 'images': images,
